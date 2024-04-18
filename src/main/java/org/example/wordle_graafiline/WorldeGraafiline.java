0a09ed3ae15a8335dc246631ce46e4018bd806d0
Alexandra Voit <alexandra.voit@ut.ee>
2024-04-18 13:26:58 +0300

    Üldine graafiline vaade

---
+++ src/main/java/org/example/wordle_graafiline/WorldeGraafiline.java
@@ -0,0 +1,76 @@
+package org.example.wordle_graafiline;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+
+public class WorldeGraafiline extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        // SUUREM PILT
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(10, 10, 10, 10));
+        vBox.setSpacing(10);
+        vBox.setAlignment(Pos.CENTER);
+
+
+        Label pealkiri = new Label("WORDLE");
+        pealkiri.setStyle("-fx-font-weight: bold; -fx-font-size: 40px; -fx-text-fill: #3d5b37;");
+        vBox.getChildren().add(pealkiri);
+
+
+        // RUUDUSTIKU LOOMINE
+        for (int i = 0; i < 8; i++) {
+            HBox hBox = new HBox();
+            hBox.setSpacing(10);
+
+            hBox.setAlignment(Pos.CENTER);
+
+            for (int j = 0; j < 6; j++) {
+                double ruuduKülg = 50;
+
+                Rectangle ruut = new Rectangle(ruuduKülg , ruuduKülg);
+                ruut.setFill(Color.LIGHTGREY);
+
+                hBox.getChildren().add(ruut);
+            }
+
+
+            vBox.getChildren().add(hBox);
+        }
+
+        TextField guess = new TextField();
+        guess.setMaxSize(100,20);
+        guess.setPadding(new Insets(10));
+        VBox.setMargin(guess, new Insets(20, 0, 0, 0));
+
+        vBox.getChildren().add(guess);
+
+
+        Scene stseen = new Scene(vBox, 800,800);
+
+        primaryStage.setScene(stseen);
+        primaryStage.setTitle("WORDLE");
+
+        vBox.prefWidthProperty().bind(stseen.widthProperty());
+        vBox.prefHeightProperty().bind(stseen.heightProperty());
+
+        primaryStage.show();
+
+    }
+}
