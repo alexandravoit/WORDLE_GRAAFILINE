3c771c89d991e96f167666262aec4d7915f8c954
Alexandra Voit <alexandra.voit@ut.ee>
2024-05-14 23:55:34 +0300

    Natukene disaini, vastuse nupp reageerib, veateade mitte 6 tähelise sõna puhul

--- src/main/java/org/example/wordle_graafiline/WorldeGraafiline.java
+++ src/main/java/org/example/wordle_graafiline/WorldeGraafiline.java
@@ -1,5 +1,6 @@
 package org.example.wordle_graafiline;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -10,15 +11,21 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 import java.util.Arrays;
 
 public class WorldeGraafiline extends Application {
 
     static Rectangle[][] ruudud;
+    static int vastused = 0;
+    static String font = "Impact";
 
     public static void main(String[] args) {
         launch(args);
@@ -35,8 +42,11 @@ public class WorldeGraafiline extends Application {
         vBox.setAlignment(Pos.CENTER);
 
 
+
         Label pealkiri = new Label("WORDLE");
-        pealkiri.setStyle("-fx-font-weight: bold; -fx-font-size: 40px; -fx-text-fill: #3d5b37;");
+        pealkiri.setTextFill(Paint.valueOf("#3d5b37"));
+        pealkiri.setFont(Font.font(font, 50));
+
         vBox.getChildren().add(pealkiri);
 
         ruudud = new Rectangle[8][6];
@@ -70,6 +80,43 @@ public class WorldeGraafiline extends Application {
         TextField guess = new TextField(); // LAHTER VASTUSE JAOKS
         Button guessButton = new Button("VASTA"); // NUPP VASTUSE SUBMITTIMISEKS
 
+
+
+        // KOM VASTUSE SISESTUS JA SELLE ANALÜÜSI OSA
+        guessButton.setOnAction(e -> {
+
+            String sisestus = guess.getText(); // KASUTAJA GUESS
+
+            if (sisestus.length() == 6) vastused ++;
+            if (sisestus.length() != 6) {
+                Text teade = new Text("sisesta 6-täheline sõna!");
+                teade.setFont(Font.font(font, 20));
+                teade.setFill(Paint.valueOf("#3d5b37"));
+
+
+                vBox.getChildren().add(teade);
+
+                // KOM VEATEADE VÄLJASTATAKSE 3ks SEKUNDIKS
+                PauseTransition paus = new PauseTransition(Duration.seconds(3));
+                paus.setOnFinished(event -> vBox.getChildren().remove(teade));
+                paus.play();
+
+            }
+
+            // MÄNGU LÕPP
+            if (vastused == 6) {
+                System.out.println("LÄBI"); // enda infoks, pole lõppversioonis vajalik
+
+                // TODO LISA MINGI MÄNGU LÕPU TEAVITUS JA STATS
+            }
+
+
+
+            System.out.println("Sisend: " + sisestus + ", kord: " + vastused);
+
+        });
+
+
         // LAHTRI JA NUPU FORMATTING
         guess.setMaxSize(100,20);
         guess.setPadding(new Insets(10));
@@ -79,9 +126,14 @@ public class WorldeGraafiline extends Application {
 
 
         Scene stseen = new Scene(vBox, 800,800);
+        stseen.setFill(Color.DARKGREEN);
+
+
 
         primaryStage.setScene(stseen);
         primaryStage.setTitle("WORDLE");
+
+
 
         vBox.prefWidthProperty().bind(stseen.widthProperty());
         vBox.prefHeightProperty().bind(stseen.heightProperty());
